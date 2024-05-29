a1c8c7ea34f48bad48d15ac6df7a7ef7e989541d
Woosung Jo <115052294+wooooooooooooooosung@users.noreply.github.com>
2024-05-29 19:14:35 +0900

    Create mysql.js

---
+++ db/mysql.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2/promise');
+const config = require('./db.config.json');
+const log = require('../func/logger');
+
+// Connection Pool 생성
+const pool = mysql.createPool(config);
+
+const doCallProcedure = async function(query, param) {
+    try {
+        let connection = await pool.getConnection(async conn => conn);
+        let result = await connection.query(query, param);
+        // Connection 반납
+        connection.release();
+        // 결과값 리턴
+        return result;
+    } catch (err) {
+        log.error(err);
+        return err;
+    }
+};
+
+module.exports = { 
+    doCallProcedure
+};
